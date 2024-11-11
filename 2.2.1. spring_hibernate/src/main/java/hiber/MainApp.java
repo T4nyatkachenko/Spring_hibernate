be5cb59e19158920e9a589762e69641737133569
T4nyatkachenko <T4nyatkachenko@yandex.ru>
2024-11-12 00:14:27 +0300

    Выполнено задание 2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,13 +1,14 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 import java.util.List;
-
+//?verifyServerCertificate=false&useSSL=false&requireSSL=false&useLegacyDatetimeCode=false&amp&serverTimezone=UTC
 public class MainApp {
    public static void main(String[] args) throws SQLException {
       AnnotationConfigApplicationContext context = 
@@ -15,10 +16,16 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+     // userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+     // userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+     // userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+     // userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("BMW", 777)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Ford", 444)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Opel", 111)));
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +33,15 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println(user.getCar().toString());
          System.out.println();
       }
+
+      System.out.println("Находим User по модели и серии Car:");
+
+      User us = userService.findUserByCar("Ford", 444);
+      System.out.println("Модель: Ford, серия: 444: ");
+      System.out.println(us.getFirstName() + ", " +  us.getLastName());
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -22,8 +23,26 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
 
+   @Override
+   public User findUserByCar(String model, int series) {
+      String hql = "select u From User u join fetch u.car WHERE u.car.model =:model AND u.car.series = :series";
+      Query query = sessionFactory.getCurrentSession().createQuery(hql);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return (User) query.getSingleResult();
+   }
 }
+
+
+
+
+
+
+
+
+
+
